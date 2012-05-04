ec1ea307343931a3ac3edf43e5d0b743754a791d
DiscoM <mk.on.ahk@gmail.com>
2012-05-04 15:35:09 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twit::Application.config.secret_token = '45c2b9fed648b83c2f6693975cb816e728f94d632e220fe86acff6731b93a3c27fbfdc41fde29bd53700dcedcafe576e403ee734dbf9ceb5376335f6b9e7434e'
